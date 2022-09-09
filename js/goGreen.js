f770cf583f7b486b3968886833c1c0145d0635f0
Egor-Gaidiuchenko <evgaidiuchenko@gmail.com>
2022-09-09 03:54:05 +0300

    tabs and burger

--- js/goGreen.js
+++ js/goGreen.js
@@ -1,1 +1,58 @@
 // Tabs
+
+const tabs = document.querySelectorAll('.tab'),
+      navItem = document.querySelectorAll('.nav-item'),
+      nav = document.querySelector('.nav'),
+      footer = document.querySelector('.footer');
+
+nav.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('nav-item')) {
+        navItem.forEach((item, i) => {
+            if (target == item) {
+                item.classList.add('nav-checked');
+                tabs[i].classList.remove('hide');
+            } else {
+                item.classList.remove('nav-checked');
+                tabs[i].classList.add('hide');
+            }
+        });
+    }
+
+    if (navItem[0].classList.contains('nav-checked')) {
+        footer.classList.add('hide');
+    } else {
+        footer.classList.remove('hide');
+    }
+});
+
+// Burger
+
+const burger = document.querySelector('.menu'),
+      burgerMenu = document.querySelector('.burger'),
+      hiddenMenu = document.querySelector('.hidden-menu'),
+      hiddenNavItem = document.querySelectorAll('.hidden-nav-item');
+      
+burger.addEventListener('click', () => {
+    burgerMenu.classList.toggle('hide');
+    document.body.classList.toggle('lock');
+});
+
+burgerMenu.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('hidden-nav-item')) {
+        hiddenNavItem.forEach((item, i) => {
+            if (target == item) {
+                item.classList.add('hidden-nav-checked');
+                tabs[i].classList.remove('hide');
+            } else {
+                item.classList.remove('hidden-nav-checked');
+                tabs[i].classList.add('hide');
+            }
+        });
+
+        burgerMenu.classList.add('hide');
+    }
+});
